08b2e8ce0ebd636d12d85a8df51304e4a44af209
Opiela <b.opiela@uniorg.de>
2018-12-10 19:53:00 +0100

    first commit

---
+++ gitrun.bat
@@ -0,0 +1,6 @@
+echo "# address-manager" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/bopiela/address-manager.git
+git push -u origin master
